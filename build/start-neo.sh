d7646f93699f60fb0eb2afb81699b8e9f222b254
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-10-24 22:20:03 +0200

    bash bangs

--- build/start-neo.sh
+++ build/start-neo.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export JRE_HOME=/usr/lib/jvm/java-8-oracle
+
 neo/bin/neo4j start
--- modules/3.0.6/install.sh
+++ modules/3.0.6/install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd neo/plugins
 wget https://www.dropbox.com/s/zxsdqqyh1n9hcsa/graphaware-neo4j-to-elasticsearch-3.0.6.43.7-SNAPSHOT.jar
 wget http://products.graphaware.com/download/framework-server-enterprise/graphaware-server-enterprise-all-3.0.6.43.jar
