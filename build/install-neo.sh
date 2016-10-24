8c7fdd0b4a33649d54c85fb9c08a95fbd38c2d93
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-10-24 21:55:49 +0200

    fix neo version in build

--- build/install-neo.sh
+++ build/install-neo.sh
@@ -3,7 +3,7 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export JRE_HOME=/usr/lib/jvm/java-8-oracle
 
-wget http://dist.neo4j.org/neo4j-enterprise-3.0.1-unix.tar.gz > null
+wget http://dist.neo4j.org/neo4j-enterprise-$NEO_VERSION-unix.tar.gz > null
 mkdir neo
 tar xzf neo4j-enterprise-$NEO_VERSION-unix.tar.gz -C neo --strip-components=1 > null
 cp build/neo4j-inclusion.conf neo/conf/neo4j.conf
