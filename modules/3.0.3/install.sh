605cbd0bff5c0671e828c124640e3906145ef7b9
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-10-24 21:29:34 +0200

    added modules install

---
+++ modules/3.0.3/install.sh
@@ -0,0 +1,4 @@
+cd neo/plugins
+wget https://www.dropbox.com/s/zxsdqqyh1n9hcsa/graphaware-neo4j-to-elasticsearch-3.0.3.39.7-SNAPSHOT.jar
+wget http://products.graphaware.com/download/framework-server-enterprise/graphaware-server-enterprise-all-3.0.3.39.jar
+wget http://products.graphaware.com/download/uuid/graphaware-uuid-3.0.3.39.10.jar
---
+++ modules/3.0.6/install.sh
@@ -0,0 +1,4 @@
+cd neo/plugins
+wget https://www.dropbox.com/s/zxsdqqyh1n9hcsa/graphaware-neo4j-to-elasticsearch-3.0.6.43.7-SNAPSHOT.jar
+wget http://products.graphaware.com/download/framework-server-enterprise/graphaware-server-enterprise-all-3.0.6.43.jar
+wget http://products.graphaware.com/download/uuid/graphaware-uuid-3.0.6.43.11.jar
