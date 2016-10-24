de6fec40983f4f19e66188c6422173bfc2853065
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-10-24 21:18:56 +0200

    initial-commit

---
+++ build/install-neo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export JRE_HOME=/usr/lib/jvm/java-8-oracle
+
+wget http://dist.neo4j.org/neo4j-enterprise-3.0.1-unix.tar.gz > null
+mkdir neo
+tar xzf neo4j-enterprise-3.0.1-unix.tar.gz -C neo --strip-components=1 > null
+cp neo4j-inclusion.conf neo/conf/neo4j.conf
+cat neo/conf/neo4j.conf
+neo/bin/neo4j start > null &
