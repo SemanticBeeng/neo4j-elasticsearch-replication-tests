2b391afd73cf3bd9fcd6caf694d9e90955df1c5d
Christophe Willemsen <willemsen.christophe@gmail.com>
2016-10-24 21:35:55 +0200

    make neo download version from en

--- build/install-neo.sh
+++ build/install-neo.sh
@@ -5,7 +5,7 @@ export JRE_HOME=/usr/lib/jvm/java-8-oracle
 
 wget http://dist.neo4j.org/neo4j-enterprise-3.0.1-unix.tar.gz > null
 mkdir neo
-tar xzf neo4j-enterprise-3.0.1-unix.tar.gz -C neo --strip-components=1 > null
-cp neo4j-inclusion.conf neo/conf/neo4j.conf
+tar xzf neo4j-enterprise-$NEO_VERSION-unix.tar.gz -C neo --strip-components=1 > null
+cp build/neo4j-inclusion.conf neo/conf/neo4j.conf
 cat neo/conf/neo4j.conf
 neo/bin/neo4j start > null &
